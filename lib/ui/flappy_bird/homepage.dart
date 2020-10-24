0bb3c844446541d893a996d8e994d09faf7a6614
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2020-10-24 15:23:38 +0800

    adjust text style

--- lib/ui/flappy_bird/homepage.dart
+++ lib/ui/flappy_bird/homepage.dart
@@ -70,21 +70,27 @@ class _HomePageState extends State<HomePage> {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('SCORE'),
+                      Text(
+                        'SCORE',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
                       SizedBox(
                         height: 20,
                       ),
-                      Text('0'),
+                      Text('0',
+                          style: TextStyle(color: Colors.white, fontSize: 35)),
                     ],
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('BEST'),
+                      Text('BEST',
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
                       SizedBox(
                         height: 20,
                       ),
-                      Text('10'),
+                      Text('10',
+                          style: TextStyle(color: Colors.white, fontSize: 35)),
                     ],
                   )
                 ],
